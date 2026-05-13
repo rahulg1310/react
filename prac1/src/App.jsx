918ac3b3f8385995a82eb58d6bf10511ab54f0bd
rahulg1310 <rahulganesan3@gmail.com>
2026-05-13 22:53:15 +0530

    prac1 added

---
+++ prac1/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div id='parent'>
+        <h1 id='child1'>Child1</h1>
+        <h1 id='child2'>Child2</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
