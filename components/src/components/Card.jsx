af49d42a69bc539479b7e4d6043e1f368cc95668
rahulg1310 <rahulganesan3@gmail.com>
2026-05-13 23:26:49 +0530

    components added

---
+++ components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <>
+      <div className="Card">
+        <h1>Rahul Ganesan</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod cumque, eos, vel eius iste mollitia officiis velit eveniet similique expedita dolore aliquid. Neque quis, corporis est doloribus debitis ad molestias.</p>
+      </div>
+    </>
+  )
+}
+
+export default Card
---
+++ components/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <div className='Navbar'>
+        <h1>This is Navbar</h1>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
