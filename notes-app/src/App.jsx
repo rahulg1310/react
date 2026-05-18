3c34c8f1e033a6a1413c20aae5a030c28d71855f
rahulg1310 <rahulganesan3@gmail.com>
2026-05-18 17:37:22 +0530

    notes-app done

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -4,13 +4,21 @@ const App = () => {
 
   const [title, setTitle] = useState('');
   const [detail, setDetail] = useState('');
+  const [task, setTask] = useState([]);
 
   const submitHandler = (e) =>{
     e.preventDefault();
-    console.log(title);
-    console.log(detail);
+    const copyTask = [...task];
+    copyTask.push({title,detail});
+    setTask(copyTask);
     setTitle('');
     setDetail('');
+  }
+
+  const delTask = (idx) =>{
+    const copyTask = [...task];
+    copyTask.splice(idx,1);
+    setTask(copyTask);
   }
 
   return (
@@ -36,14 +44,20 @@ const App = () => {
             setDetail(e.target.value);
           }} ></textarea>
           <button 
-          className='bg-white w-full text-black px-5 py-2 font-medium border-2 outline-none rounded'>Add Note</button>
+          className='bg-white w-full active:scale-95 text-black px-5 py-2 font-medium border-2 outline-none rounded'>Add Note</button>
       </form>
       <div className=' p-10 lg:w-1/2 lg:border-l-2'>
         <h1 className='text-4xl font-bold'>Recent Notes</h1>
-        <div className='flex flex-wrap gap-5 mt-5 overflow-auto h-full'>
-          <div className='h-52 w-40 bg-white rounded-2xl'></div>
-          <div className='h-52 w-40 bg-white rounded-2xl'></div>
-          <div className='h-52 w-40 bg-white rounded-2xl'></div>    
+        <div className='flex flex-wrap  gap-5 mt-5 overflow-auto h-[90%]'>
+          {task.map(function(elem,idx){
+            return <div className='flex flex-col h-52 w-40 bg-white rounded-2xl text-black py-6 px-4'>
+              <h3 className='font-bold mb-3'>{elem.title}</h3>
+              <p className='text-xs text-gray-500'>{elem.detail}</p>
+              <button onClick={()=>{
+                delTask(idx);
+              }} className='w-full bg-red-400 items-end active:scale-95 py-1 rounded mt-auto text-white'>Delete</button>
+            </div>
+          })}
         </div>
       </div>
     </div>
